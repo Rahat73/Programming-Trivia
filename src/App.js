4571964a94a7c3768788aa31e08a16385a8c67f4
Rahat73 <55106915+Rahat73@users.noreply.github.com>
2022-10-12 14:41:40 +0600

    route created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './Components/Blog/Blog';
+import Home from './Components/Home/Home';
+import Statistics from './Components/Statistics/Statistics';
 import Main from './Layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          // loader: () => fetch('products.json'),
+          element: <Home></Home>
+        },
+        {
+          path: 'orders',
+          // loader: productsAndCartLoader,
+          element: <Statistics></Statistics>
+        },
+        {
+          path: 'inventory',
+          element: <Blog></Blog>
+        }
+      ]
+    }
+  ])
   return (
-    <div className="App">
-      <Main></Main>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,27 +1,70 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <div className="navbar bg-base-300 justify-between">
+            <div className="navbar bg-base-300 justify-between py-5 shadow-lg">
                 <div className="mx-14">
                     <div className="dropdown">
-                        <label tabindex="0" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Home</a></li>
-                            <li><a>Statistics</a></li>
-                            <li><a>Blog</a></li>
+                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><NavLink
+                                to="/"
+                                className={isActive =>
+                                    "nav-link" + (!isActive ? " unselected" : "")
+                                }
+                            >
+                                Home
+                            </NavLink></li>
+                            <li><NavLink
+                                to="/"
+                                className={isActive =>
+                                    "nav-link" + (!isActive ? " unselected" : "")
+                                }
+                            >
+                                Statistics
+                            </NavLink></li>
+                            <li><NavLink
+                                to="/"
+                                className={isActive =>
+                                    "nav-link" + (!isActive ? " unselected" : "")
+                                }
+                            >
+                                Blog
+                            </NavLink></li>
                         </ul>
                     </div>
-                    <button className="w-max font-bold text-transparent text-2xl lg:text-4xl bg-clip-text bg-gradient-to-r from-yellow-200 to-purple-300 py-1 mx-4">Programming-Trivia</button>
+                    <button className="font-mono font-bold text-transparent text-2xl lg:text-4xl bg-clip-text bg-gradient-to-r from-yellow-200 to-purple-300 py-1 mx-4">Programming-Trivia</button>
                 </div>
                 <div className="navbar-center hidden lg:flex mx-20">
                     <ul className="menu menu-horizontal p-0">
-                        <li><a>Home</a></li>
-                        <li><a>Statistics</a></li>
-                        <li><a>Blog</a></li>
+                        <li><NavLink
+                            to="/"
+                            className={isActive =>
+                                "nav-link" + (!isActive ? " unselected" : "")
+                            }
+                        >
+                            Home
+                        </NavLink></li>
+                        <li><NavLink
+                            to="/"
+                            className={isActive =>
+                                "nav-link" + (!isActive ? " unselected" : "")
+                            }
+                        >
+                            Statistics
+                        </NavLink></li>
+                        <li><NavLink
+                            to="/"
+                            className={isActive =>
+                                "nav-link" + (!isActive ? " unselected" : "")
+                            }
+                        >
+                            Blog
+                        </NavLink></li>
                     </ul>
                 </div>
             </div>
