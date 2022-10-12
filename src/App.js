b9b5382882c108acd95e57915396df0dfa260570
Rahat73 <55106915+Rahat73@users.noreply.github.com>
2022-10-12 22:54:38 +0600

    blog  updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Blog from './Components/Blog/Blog';
+import Error from './Components/Error/Error';
 import Home from './Components/Home/Home';
 import Quiz from './Components/Quiz/Quiz';
 import Statistics from './Components/Statistics/Statistics';
@@ -31,6 +32,10 @@ function App() {
           path: '/quiz/:id',
           loader: async ({ params }) => fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`),
           element: <Quiz></Quiz>
+        },
+        {
+          path: '*',
+          element: <Error></Error>
         }
       ]
     }
