aaf38ee14297c4827990448f95442065912d1760
Rahat73 <55106915+Rahat73@users.noreply.github.com>
2022-10-12 21:49:18 +0600

    header updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import { useLoaderData } from 'react-router-dom';
 import Topics from '../Topics/Topics';
+import image from '../../../src/7167569.jpg'
 
 const Home = () => {
     const topics = useLoaderData().data;
 
     return (
         <div className='w w-4/5 mx-auto my-14'>
+            <img src={image} alt="" />
             <div className="grid grid-cols-1 lg:grid-cols-4 lg:gap-4">
                 {
                     topics.map(topic => <Topics key={topic.id} topic={topic}></Topics>)
--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -14,6 +14,10 @@ const Question = ({ question }) => {
             toast("Wrong answer.. Try Again");
         }
     }
+
+    const revealAns = (answer) => {
+        toast(answer);
+    }
     return (
         <div className='my-5'>
             <ToastContainer />
@@ -21,7 +25,7 @@ const Question = ({ question }) => {
                 <div>
                     <h1 className='my-5 font-semibold text-xl'>Question: <span>{question.question}</span></h1>
                     <div className="flex justify-end mr-3">
-                        <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <button onClick={() => revealAns(question.correctAnswer)}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg></button>
