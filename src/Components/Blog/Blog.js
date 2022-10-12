474d6950314afe6b6e90bd3db0facd9c9e012406
Rahat73 <55106915+Rahat73@users.noreply.github.com>
2022-10-12 22:48:33 +0600

    404 issue solved

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -13,7 +13,14 @@ const Blog = () => {
             </div>
             <div>
                 <h1 className='text-2xl font-semibold my-3'>What is the purpose of the react router?</h1>
-                <p className='px-3'>-- React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.</p>
+                <p className='px-3'>-- The useRef Hook allows you to persist values between renders.
+
+                    It can be used to store a mutable value that does not cause a re-render when updated.
+
+                    It can be used to access a DOM element directly.
+                    If we tried to count how many times our application renders using the useState Hook, we would be caught in an infinite loop since this Hook itself causes a re-render. useRef() only returns one item. It returns an Object called current.
+
+                    When we initialize useRef we set the initial value: useRef(0).</p>
             </div>
         </div>
     );
